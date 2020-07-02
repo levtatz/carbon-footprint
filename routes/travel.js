fefe8d744c62097fff138107d454c4885a51fd93
Lev Tatz <lev.tatz@gmail.com>
2020-07-02 17:59:32 -0400

    add validation to travel

--- routes/travel.js
+++ routes/travel.js
@@ -1,17 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const { body, validationResult } = require('express-validator');
+
 const travelEmissionFactors = require('../emission-factors/travel.json');
 const kgToLbsFactor = 2.205;
 
 /* GET travel emissions. */
-router.get('/', function (req, res, next) {
-  const miles = req.body.miles;
-  const type = req.body.type;
-  const emissionFactor = travelEmissionFactors.find(
-    (travelEmission) => travelEmission.type == type
-  );
-  res.json({ emissions: emissionFactor.CO2 * kgToLbsFactor * miles });
-});
+router.get(
+  '/',
+  [
+    body('type').isAlpha().withMessage('type must be a string'),
+    body('miles')
+      .isInt({ min: 1 })
+      .withMessage('miles must be a positive number'),
+  ],
+  function (req, res, next) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(422).json({ errors: errors.array() });
+    }
+
+    const miles = req.body.miles;
+    const type = req.body.type;
+    const emissionFactor = travelEmissionFactors.find(
+      (travelEmission) => travelEmission.type == type
+    );
+    res.json({ emissions: emissionFactor.CO2 * kgToLbsFactor * miles });
+  }
+);
 
 module.exports = router;
