9aa08f30eadacf9e69cda4b708f66e413002aee8
Lev Tatz <lev.tatz@gmail.com>
2020-07-03 14:29:08 -0400

    add layout and travel form

--- web/src/App.js
+++ web/src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Layout, Menu } from 'antd';
+import { TravelForm } from './components/TravelForm';
+
+import 'antd/dist/antd.css';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  const { Header, Content } = Layout;
+  return (
+    <Layout className="layout">
+      <Header>
+        <Menu mode="horizontal" defaultSelectedKeys={['1']}>
+          <Menu.Item key="1">💼</Menu.Item>
+          <Menu.Item key="2">🚗</Menu.Item>
+          <Menu.Item key="3">🏠</Menu.Item>
+        </Menu>
+      </Header>
+      <Content style={{ padding: '50px' }}>
+        <div className="site-layout-content">
+          <TravelForm />
+        </div>
+      </Content>
+    </Layout>
+  );
 }
 
 export default App;
---
+++ web/src/components/TravelForm.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Form, InputNumber, Button, Select } from 'antd';
+
+const { Option } = Select;
+
+const vehicleTypes = [
+  'Passenger Car',
+  'Light-Duty Truck',
+  'Motorcycle',
+  'Intercity Rail - Amtrak',
+  'Commuter Rail',
+  'Transit Rail (i.e. Subway, Tram)',
+  'Bus',
+  'Air Travel - Short Haul (< 300 miles)',
+  'Air Travel - Medium Haul (>= 300 miles, < 2300 miles)',
+  'Air Travel - Long Haul (>= 2300 miles)',
+];
+
+const layout = {
+  labelCol: {
+    span: 8,
+  },
+  wrapperCol: {
+    span: 16,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 8,
+    span: 16,
+  },
+};
+
+export const TravelForm = () => {
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Form {...layout} form={form} name="control-hooks" onFinish={onFinish}>
+      <Form.Item
+        name="type"
+        label="Vehicle"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <Select placeholder="Select a vehicle type" allowClear>
+          {vehicleTypes.map((vehicleType) => (
+            <Option value={vehicleType}>{vehicleType}</Option>
+          ))}
+        </Select>
+      </Form.Item>
+
+      <Form.Item
+        name="miles"
+        label="Miles"
+        rules={[
+          {
+            required: true,
+          },
+        ]}
+      >
+        <InputNumber />
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Calculate Carbon Footprint
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+};
