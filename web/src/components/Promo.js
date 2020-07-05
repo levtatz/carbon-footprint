9e5eb5a3022da068c2d8a750c566b3e9bb6d6a37
Lev Tatz <lev.tatz@gmail.com>
2020-07-05 19:09:24 -0400

    add arcadia promo

--- web/src/components/Promo.js
+++ web/src/components/Promo.js
@@ -1,24 +1,52 @@
 import React from 'react';
 import { Card } from 'antd';
 
-export const Promo = ({ emissions, tabKey }) =>
-  emissions && tabKey === '3' ? (
-    <Card
-      title="Neutralize Your Drive                "
-      extra={
-        <a href="https://www.aspiration.com/" target="_blank">
-          More
-        </a>
-      }
-      style={{ maxWidth: 333 }}
-    >
-      <p>
-        <a href="https://www.aspiration.com/" target="_blank">
-          Aspiration’s
-        </a>{' '}
-        Planet Protection™ feature tallies up the carbon output of all of your
-        gas purchases, then automatically buys offsets to help counter the
-        climate impact.
-      </p>
-    </Card>
-  ) : null;
+export const Promo = ({ emissions, tabKey }) => {
+  if (!emissions) return null;
+
+  switch (tabKey) {
+    case '1':
+      return (
+        <Card
+          title="Clean up your electricity"
+          extra={
+            <a href="https://www.arcadia.com/" target="_blank">
+              More
+            </a>
+          }
+          style={{ maxWidth: 333 }}
+        >
+          <p>
+            Connect your utility account to{' '}
+            <a href="https://www.arcadia.com/" target="_blank">
+              Arcadia
+            </a>{' '}
+            to access clean energy and lower power bills in under two minutes.
+          </p>
+        </Card>
+      );
+    case '3':
+      return (
+        <Card
+          title="Neutralize your drive"
+          extra={
+            <a href="https://www.aspiration.com/" target="_blank">
+              More
+            </a>
+          }
+          style={{ maxWidth: 333 }}
+        >
+          <p>
+            <a href="https://www.aspiration.com/" target="_blank">
+              Aspiration’s
+            </a>{' '}
+            Planet Protection™ feature tallies up the carbon output of all of
+            your gas purchases, then automatically buys offsets to help counter
+            the climate impact.
+          </p>
+        </Card>
+      );
+    default:
+      return null;
+  }
+};
